d2e952464321a3d62c56e7f7218ef92100c265c8
João <jvmmoraesdev@gmail.com>
2025-04-30 00:45:38 -0300

    entrada de dados em C

---
+++ entrada_em_c.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+    int idade;
+
+    printf("Ola Mundo! Digite sua idade: ");
+    scanf("%i",&idade);
+    printf("Olá, voce possui %i anos de idade!",idade);
+
+
+
+    return 0;
+}
