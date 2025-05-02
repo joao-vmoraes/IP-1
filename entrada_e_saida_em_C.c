84314be283cf8e96caee8a1f1bf7587dfaa0a532
João <jvmmoraesdev@gmail.com>
2025-05-02 14:45:54 -0300

    perguntando altura em c

---
+++ entrada_e_saida_em_C.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+    float altura;
+    printf("Ola pequeno gafanhoto! Qual o sua altura em metros? ");
+    scanf("%f", &altura);
+    printf("Bom saber que voce tem %f Metros!", altura);
+
+    return 0;
+}
