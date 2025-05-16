b49db20aba38a5701eef117a830e0ff8bee8da22
João Vitor Moraes de Andrade <jvmmoraesdev@gmail.com>
2025-05-16 13:32:12 -0300

    (quadrado.c)  adicionado, quarta questão da prova

---
+++ Prova da AV1/quadrado.c
@@ -0,0 +1,28 @@
+//desenhar um quadrado de lado x e sua diagonal 
+//linha = l, coluna = c
+#include <stdio.h>
+
+int main(){
+    int lado;
+    
+    printf("Escolha o lado de um quadrado: ");
+    scanf("%i", &lado);
+    
+
+    for (int c = 1; c <= lado; c++)
+    {   
+        for ( int l = 1; l <= lado; l++)
+            {
+                if (c == 1 ||  l == 1 || l == lado  || c == lado  || l == c || l + c == lado + 1) // tive que pesquisar sobre essa propriedade da diagonal secundaria  ' l + c = lado + 1 '
+                {
+                    printf("[]");
+                }
+                
+                else
+                {
+                    printf("  ");
+                }
+            }
+        printf("\n");
+    }
+}
