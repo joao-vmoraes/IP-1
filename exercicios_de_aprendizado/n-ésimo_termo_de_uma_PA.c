ed3f2dc0c0664f91b33e4efd6c7f2014a3f720eb
João Vitor Moraes de Andrade <jvmmoraesdev@gmail.com>
2025-05-23 22:46:18 -0300

    'n-ésim_termo_de_uma_PA.c' adicionado!

---
+++ exercicios_de_aprendizado/n-ésimo_termo_de_uma_PA.c
@@ -0,0 +1,20 @@
+//somas dos termos de uma pa
+#include <stdio.h>
+
+int main(void){
+    int a1, n, razao, an = a1 * (n - 1) + razao;
+    while(1)
+    {
+        printf("===Analizador de PA===");
+        printf("\nEscolha o primeiro termo: ");
+        scanf(" %i", &a1);
+
+        printf("\nEscolha o n-ésimo termo que você quer saber: ");
+        scanf(" %i", &n);
+        
+        printf("\nEscolha agora a razão da PA: ");
+        scanf(" %i", &razao);
+
+        printf("O valor do termo de número %i é %i", n, an);
+    }
+}
