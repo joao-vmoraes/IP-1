2e2fafebfb0c15fe26ea04d79ada6553dd16b067
João Vitor Moraes de Andrade <jvmmoraesdev@gmail.com>
2025-05-24 10:44:56 -0300

    erro no 'n-ésimo termo de uma PA.c' atualizado

--- exercicios_de_aprendizado/n-ésimo_termo_de_uma_PA.c
+++ exercicios_de_aprendizado/n-ésimo_termo_de_uma_PA.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 int main(void){
-    int a1, n, razao, an = a1 * (n - 1) + razao;
+    int a1, n, razao, escolha;
     while(1)
     {
-        printf("===Analizador de PA===");
+        printf("\n===Analizador de PA===");
         printf("\nEscolha o primeiro termo: ");
         scanf(" %i", &a1);
 
@@ -14,7 +14,18 @@ int main(void){
         
         printf("\nEscolha agora a razão da PA: ");
         scanf(" %i", &razao);
+        
+        int an = a1 + (n - 1) * razao;
+        printf("O valor do termo de posição %i° é %i", n, an);
 
-        printf("O valor do termo de número %i é %i", n, an);
+        printf("\nDeseja fazer uma nova análise?\n [1] = sim, [2] = não  : ");
+        scanf(" %i", &escolha);
+        switch(escolha){
+            case 2:
+                return 0;
+            default:
+                break;
+        }
+
     }
 }
