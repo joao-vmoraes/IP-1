ed84f93587b52a1e7a835ba8f936f32e96c8638c
João Vitor Moraes de Andrade <jvmmoraesdev@gmail.com>
2025-05-31 12:46:24 -0300

    'vetor_introducao.c' adicionado

---
+++ exercicios_de_aprendizado/vetor_introducao.c
@@ -0,0 +1,5 @@
+//um veto recebe seu tipo(int,char,float), seu indentificado(nome) e sua quantidade[quantidade]  ex: int numeros[10], o vetor numeros recebe 10 valores.
+
+int main(void){
+    char palavra[20];
+}
