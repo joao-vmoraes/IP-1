19306bcf64da1b8489099ccbf61d22285458f1db
joao <jvmmoraesdev@gmail.com>
2025-07-29 13:27:47 -0300

    'aluguel.c' finalizado e adicionado com sucesso

--- Prova/aluguel.c
+++ Prova/aluguel.c
@@ -50,8 +50,23 @@ int adicionar(Filme *filmes) {
         }
     }
 
-    return 1;
+    return 1; //filmme adicioado
 }
+
+int buscar(Filme* filmes){
+    char cod[20];
+    printf("Digite o código do filme para buscar:\n>>> ");
+    fgets(cod, 20, stdin);
+    cod[strcspn(cod, "\n")] = 0;
+    
+    for (int i = 0; i < 100; i++) {
+        if (filmes[i].status == 1 && strcmp(filmes[i].codigo_filme, cod) == 0) {
+            return i; 
+        }
+    }
+    return -1; 
+}
+
 
 void listar(Filme *filmes) {
     for (int i = 0; i < 100; i++) {
@@ -132,6 +147,100 @@ int devolver(Filme *filmes) {
     return 0;
 }
 
+int comparar_alugueis(const void *a, const void *b) {
+    Filme *f1 = (Filme *)a;
+    Filme *f2 = (Filme *)b;
+    return f2->n_alugueis - f1->n_alugueis;
+}
+
+void ListarMaisAlugados(Filme *filmes) {
+    Filme copia[100];
+    int tam = 0;
+
+    for (int i = 0; i < 100; i++) {
+        if (filmes[i].status == 1 && strcmp(filmes[i].codigo_filme, "0") != 0) {
+            copia[tam++] = filmes[i];
+        }
+    }
+
+    qsort(copia, tam, sizeof(Filme), comparar_alugueis);
+
+    printf("\n=== FILMES MAIS ALUGADOS ===\n");
+    for (int i = 0; i < tam; i++) {
+        printf("\n==== %dº %s ====\n", i + 1, copia[i].titulo);
+        printf("Codigo: %s\n", copia[i].codigo_filme);
+        printf("Disponiveis: %d\n", copia[i].quantidade_disponivel);
+        printf("Alugueis: %d\n", copia[i].n_alugueis);
+        printf("Valor: R$%.2f\n", copia[i].preco_aluguel);
+    }
+}
+
+int comparar_devolucoes(const void *a, const void *b) {
+    Filme *f1 = (Filme *)a;
+    Filme *f2 = (Filme *)b;
+    return f2->n_devolucoes - f1->n_devolucoes;
+}
+
+
+void listarMaisDevolvidos(Filme *filmes, int tamanho) {
+    Filme copia[100]; 
+    int tam = 0;
+
+    for (int i = 0; i < tamanho; i++) {
+        if (filmes[i].status == 1 && strcmp(filmes[i].codigo_filme, "0") != 0) {
+            copia[tam++] = filmes[i];
+        }
+    }
+
+    qsort(copia, tam, sizeof(Filme), comparar_devolucoes);
+
+    printf("\n=== FILMES MAIS DEVOLVIDOS ===\n");
+    if (tam == 0) {
+        printf("Nenhum filme ativo para listar.\n");
+    } else {
+        for (int i = 0; i < tam; i++) {
+            printf("\n==== %dº %s ====\n", i + 1, copia[i].titulo);
+            printf("Codigo: %s\n", copia[i].codigo_filme);
+            printf("Disponiveis: %d\n", copia[i].quantidade_disponivel);
+            printf("Devolucoes: %d\n", copia[i].n_devolucoes);
+            printf("Valor: R$%.2f\n", copia[i].preco_aluguel);
+        }
+    }
+}
+
+
+void extratoFinanceiro(Filme *filmes, int tamanho) {
+    float total_arrecadado = 0.0;
+    
+    for (int i = 0; i < tamanho; i++) {
+    
+        if (filmes[i].status == 1 && strcmp(filmes[i].codigo_filme, "0") != 0) {
+             total_arrecadado += (filmes[i].n_alugueis * filmes[i].preco_aluguel);
+        }
+    }
+    printf("\n=== EXTRATO FINANCEIRO ===\n");
+    printf("Total arrecadado com alugueis: R$%.2f\n", total_arrecadado);
+}
+
+
+void listarEstoque(Filme *filmes, int tamanho) {
+    printf("\n=== FILÇMES DISPONIVEIS EM ESTOQUE ===\n");
+    int count_disponiveis = 0;
+    for (int i = 0; i < tamanho; i++) {
+        if (filmes[i].status == 1 && strcmp(filmes[i].codigo_filme, "0") != 0 && filmes[i].quantidade_disponivel > 0) {
+            printf("\n==== %s ====\n", filmes[i].titulo);
+            printf("Codigo: %s\n", filmes[i].codigo_filme);
+            printf("Disponiveis: %d\n", filmes[i].quantidade_disponivel);
+            printf("Valor: R$%.2f\n", filmes[i].preco_aluguel);
+            count_disponiveis++;
+        }
+    }
+    if (count_disponiveis == 0) {
+        printf("Nenhum filme disponivel em estoque no momento.\n");
+    }
+}
+
+
 int main(void) {
     Filme filmes[100];
     float extrato = 0;
@@ -147,6 +256,7 @@ int main(void) {
     }
 
     int opcao = 0;
+    int tamanho_array_filmes = 100; 
 
     while (1) {
         do {
@@ -156,11 +266,15 @@ int main(void) {
             printf("[3] - Listar filmes\n");
             printf("[4] - Alugar filme\n");
             printf("[5] - Devolver filme\n");
-            printf("[6] - Sair\n");
+            printf("[6] - Filmes Mais Alugados\n"); 
+            printf("[7] - Filmes Mais Devolvidos\n");
+            printf("[8] - Extrato Financeiro\n");    
+            printf("[9] - Listar Estoque Disponivel\n");
+            printf("[10] - Sair\n");
             printf(">>> ");
             scanf("%d", &opcao);
             getchar();
-        } while (opcao < 1 || opcao > 6);
+        } while (opcao < 1 || opcao > 10); 
 
         switch (opcao) {
             case 1:
@@ -179,7 +293,22 @@ int main(void) {
                 devolver(filmes);
                 break;
             case 6:
-                printf("Extrato final: R$ %.2f\n", extrato);
+                ListarMaisAlugados(filmes);
+                break;
+            case 7:
+                listarMaisDevolvidos(filmes, tamanho_array_filmes); 
+                break;
+            case 8:
+                
+                printf("\n=== EXTRATO FINANCEIRO ===\n");
+                printf("Total arrecadado com alugueis: R$%.2f\n", extrato);
+                
+                break;
+            case 9:
+                listarEstoque(filmes, tamanho_array_filmes); 
+                break;
+            case 10: 
+                printf("Extrato final: R$ %.2f\n", extrato); 
                 printf("Saindo...\n");
                 return 0;
         }
